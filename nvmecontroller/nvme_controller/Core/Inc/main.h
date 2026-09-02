db297d6c0e9eb2ad80f13045be20f09f0483a684
rahulr-embedded <itsrahulr7@gmail.com>
2026-09-02 15:11:25 +0530

    Initial commit: NVMe hot-plug thermal controller

---
+++ nvmecontroller/nvme_controller/Core/Inc/main.h
@@ -0,0 +1,115 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32c0xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define ACK_BUTTON_Pin GPIO_PIN_13
+#define ACK_BUTTON_GPIO_Port GPIOC
+#define RCC_OSCX_IN_Pin GPIO_PIN_14
+#define RCC_OSCX_IN_GPIO_Port GPIOC
+#define RCC_OSCX_OUT_Pin GPIO_PIN_15
+#define RCC_OSCX_OUT_GPIO_Port GPIOC
+#define RCC_OSC_IN_Pin GPIO_PIN_0
+#define RCC_OSC_IN_GPIO_Port GPIOF
+#define RCC_OSC_OUT_Pin GPIO_PIN_1
+#define RCC_OSC_OUT_GPIO_Port GPIOF
+#define VCP_USART2_RX_Pin GPIO_PIN_3
+#define VCP_USART2_RX_GPIO_Port GPIOA
+#define SLOT4_PRESENCE_Pin GPIO_PIN_2
+#define SLOT4_PRESENCE_GPIO_Port GPIOB
+#define SLOT4_PRESENCE_EXTI_IRQn EXTI2_3_IRQn
+#define SLOT1_PRESENCE_Pin GPIO_PIN_10
+#define SLOT1_PRESENCE_GPIO_Port GPIOB
+#define SLOT1_PRESENCE_EXTI_IRQn EXTI4_15_IRQn
+#define SLOT1_COMM_OK_Pin GPIO_PIN_11
+#define SLOT1_COMM_OK_GPIO_Port GPIOB
+#define SLOT2_COMM_OK_Pin GPIO_PIN_12
+#define SLOT2_COMM_OK_GPIO_Port GPIOB
+#define SLOT3_COMM_OK_Pin GPIO_PIN_13
+#define SLOT3_COMM_OK_GPIO_Port GPIOB
+#define SLOT4_COMM_OK_Pin GPIO_PIN_14
+#define SLOT4_COMM_OK_GPIO_Port GPIOB
+#define SLOT_POWER_EN_Pin GPIO_PIN_15
+#define SLOT_POWER_EN_GPIO_Port GPIOB
+#define SLOT2_PRESENCE_Pin GPIO_PIN_9
+#define SLOT2_PRESENCE_GPIO_Port GPIOA
+#define SLOT2_PRESENCE_EXTI_IRQn EXTI4_15_IRQn
+#define EVENT_STROBE_Pin GPIO_PIN_6
+#define EVENT_STROBE_GPIO_Port GPIOC
+#define BUZZER_Pin GPIO_PIN_7
+#define BUZZER_GPIO_Port GPIOC
+#define DEBUG_SWDIO_Pin GPIO_PIN_13
+#define DEBUG_SWDIO_GPIO_Port GPIOA
+#define DEBUG_SWCLK_Pin GPIO_PIN_14
+#define DEBUG_SWCLK_GPIO_Port GPIOA
+#define SLOT3_PRESENCE_Pin GPIO_PIN_15
+#define SLOT3_PRESENCE_GPIO_Port GPIOA
+#define SLOT3_PRESENCE_EXTI_IRQn EXTI4_15_IRQn
+#define ALARM_LED_Pin GPIO_PIN_5
+#define ALARM_LED_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
